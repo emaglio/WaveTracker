f4439b0ee2f2d826ff55d8df685d656bc4f059a0
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2013-10-11 10:59:39 +1100

    Update 20131010224627_create_users.rb

--- db/migrate/20131010224627_create_users.rb
+++ db/migrate/20131010224627_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :myname, :surname, :birthdate, :location, :gender, :email, :password
+      
       t.timestamps
     end
   end
