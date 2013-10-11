41d11ff835a4f677f19db3f791babbd7d460f27a
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2013-10-11 11:13:53 +1100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessor :myname, :surname, :birth_date, :location, :gender, :email, :password
   
   validates :email, :presence => true
-  validates :password, :presence => true, :include => { :in => 0..9, :lenght => {:minimum => 3, :maximum => 15}}
+  validates :password, :presence => true, :includes => { :in => 0..9, :lenght => {:minimum => 3, :maximum => 15}}
   
   def initilize(options = {})
     @myname = options[:myname]
